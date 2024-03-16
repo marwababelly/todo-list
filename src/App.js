b0f1072bf54b82825ae1c4e5629d0da0cdb0d6b4
Marwa Babelly <marwaababelly@gmail.com>
2024-03-16 15:33:44 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, age) => {
+    setUsersList((prevUser) => {
+      return [
+        ...prevUser,
+        { name: uName, age: age, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
